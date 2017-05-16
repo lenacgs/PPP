eabe450d0a26c3d8810e39f756959f391a2b786b
lenacgs <lenacgs@gmail.com>
2017-05-16 13:17:15 +0100

    Update inserir_disciplina.c

--- inserir_disciplina.c
+++ inserir_disciplina.c
@@ -1,4 +1,4 @@
-void inserir_disciplina(Next_disciplina lista_disciplinas, Next_disciplina disciplina) {
+void insere_disciplina(Next_disciplina lista_disciplinas, Next_disciplina disciplina) {
   // Assumir que list está em global
   Next_disciplina aux = lista_disciplinas;
   while (aux->next != NULL) {
