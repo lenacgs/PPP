ba24e4ca90df3204df0e3563818029569fad1e0a
lenacgs <lenacgs@gmail.com>
2017-05-16 13:50:54 +0100

    Create imprime_num_aluno.c
    Imprime secção num_aluno de cada nó da lista_alunos.

---
+++ imprime_num_aluno.c
@@ -0,0 +1,10 @@
+void imprime_num_aluno(Next_aluno lista) {
+  Next_aluno aux = lista->next;
+  int i=1;
+
+  while(aux != NULL) {
+    printf("%d. %d\n", i, aux->num_aluno);
+    i++;
+    aux = aux->next;
+  }
+}
