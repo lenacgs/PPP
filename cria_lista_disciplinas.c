bff81a9416f15a7416a4d07f3a749f81030263d7
lenacgs <lenacgs@gmail.com>
2017-05-18 15:42:57 +0100

    Create cria_lista_disciplinas.c

---
+++ cria_lista_disciplinas.c
@@ -0,0 +1,10 @@
+Next_disciplina cria_lista_disciplinas() {
+    Next_disciplina aux;
+    aux = (Next_disciplina)malloc(sizeof(Node_disciplina));
+    if (aux != NULL) {
+        aux->docente = NULL;
+        aux->nome = NULL;
+        aux->next = NULL;
+    }
+    return aux;
+}
