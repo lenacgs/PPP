687c8d765c768c56c686aa1b67ad70da60a212c2
lenacgs <lenacgs@gmail.com>
2017-05-18 15:42:14 +0100

    Delete insere_disciplina.c

--- insere_disciplina.c
+++
@@ -1,9 +0,0 @@
-void insere_disciplina(Next_disciplina lista_disciplinas, Next_disciplina disciplina) {
-  Next_disciplina aux = lista_disciplinas;
-  while (aux->next != NULL) {
-    aux = aux->next;
-  }
-  aux->nome = disciplina->nome;
-  aux->docente = disciplina->docente;
-  aux->next = NULL;
-}
