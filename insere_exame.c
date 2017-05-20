e5a1df0044dc76c0b19c08a90cc3f9772761e1fa
lenacgs <lenacgs@gmail.com>
2017-05-20 17:56:24 +0100

    Create insere_exame.c

---
+++ insere_exame.c
@@ -0,0 +1,8 @@
+void insere_exame(Next_exame lista_exames, Next_exame exame) {
+  Next_exame aux = lista_exames;
+  while (aux->next != NULL) {
+    aux = aux->next;
+  }
+  aux->next = exame;
+  exame->next = NULL;
+}
