1e21e4df630033e3564b6dce9f17ab53895f108c
lenacgs <lenacgs@gmail.com>
2017-05-20 17:17:53 +0100

    Create cria_lista_exames.c

---
+++ cria_lista_exames.c
@@ -0,0 +1,18 @@
+Next_exame cria_lista_exames() {
+    Next_exame aux;
+    aux = (Next_exame)malloc(sizeof(Node_exame));
+    if (aux != NULL) {
+      aux->disciplina = NULL;
+      aux->epoca = NULL;
+      aux->data.dia = 0;
+      aux->data.mes = 0;
+      aux->data.ano = 0;
+      aux->hora.horas = 0;
+      aux->hora.mins = 0;
+      aux->duracao = 0;
+      aux->sala = NULL;
+      aux->inscritos = NULL;
+      aux->next = NULL;
+    }
+    return aux;
+}
