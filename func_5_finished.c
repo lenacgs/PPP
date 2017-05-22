7023c672772c303c125b15a022c23e427eddf38a
inesamaro <ines.amaro.316@gmail.com>
2017-05-22 13:47:53 +0100

    Update func_5_finished.c

--- func_5_finished.c
+++ func_5_finished.c
@@ -2,73 +2,57 @@
 
 void imprimir_exames(Next_exame lista) {
   Next_exame l;
-  int i=1;
-  char c, *ptr, *disc, *ep, *doc;
+  int i=1, c;
+  char *ptr, *disc, *ep, *doc;
   Next_ptrs_aluno inscritos;
+
   l = lista->next;
-
-
+  printf("Lista de exames existentes:\n");
   while (l) { //lista os exames existentes na lista, juntamente com a epoca
-    /*ptr = (l->disciplina)->nome;
-    printf("\n%d. ", i);
-    imprime_array(ptr);*/
-    printf("%s", l->disciplina->nome);
+    printf("%d. %s", i, l->disciplina->nome);
     i++;
-    /*ptr = l->epoca;
-    printf (", ");
-    imprime_array(ptr);*/
-    printf(", %s", l->epoca):
+    printf(", %s\n", l->epoca);
     l = l->next;
   }
+  /*o exame devia ser identificado pelo id*/
   getchar();
   printf("\nEscolha o exame: ");
-  l = lista->next;
-  c = getchar();
+  scanf("%d", &c);
+
+  l = lista;
   for (i=0; i<c; i++) {
+    printf("ciclo:\n");
+    printf("id: %d\n", l->id);
     l = l->next;
   }
-  printf("Informacao desse exame: ");
-  //imprime a info do exame escolhido
+  printf("Informacao desse exame:\n ");
   printf("\nDisciplina: ");
-  /*disc = (l->disciplina)->nome;
-  imprime_array(disc);*/
   printf("%s", l->disciplina->nome);
 
   printf("\nDocente: ");
-  /*doc = (l->disciplina)->docente;
-  imprime_array(doc);*/
   printf("%s", l->disciplina->docente);
 
   printf("\nÉpoca: ");
-  /*ep = l->epoca;
-  imprime_array(ep);*/
   printf("%s", l->epoca);
 
   printf("\nData: ");
-  printf("%d - %d - %d", l->(data->dia), l->(data->mes), l->(data->ano));
+  printf("%d - %d - %d", l->data.dia, l->data.mes, l->data.ano);
 
   printf("\nHora: ");
-  printf("%dh %d", l->(hora->horas), l->(hora->mins));
+  printf("%dh%d", l->hora.horas, l->hora.mins);
 
   printf("\nSala: ");
-  /*sala = l->sala;
-  imprime_array(sala);*/
   printf("%s", l->sala);
 
   printf("\nDuração: %d minutos", l->duracao);
 
   printf("\nAlunos inscritos: \n");
-  inscritos = l->inscritos;
-  while (l->inscritos->next != NULL) {
-    printf("%d. %d\n ", i, l->inscritos->aluno->num_aluno); //imprime o numero de aluno de cada aluno inscrito
+  /*este while nao funiona*/
+
+  inscritos = lista->inscritos->next;
+  while (inscritos != NULL) {
+    printf("%d. %d\n ", i, inscritos->aluno->num_aluno);
+    i++;
+    inscritos = inscritos->next;
   }
-
-
-    }
-
-  }
-
-
-
-
 }
