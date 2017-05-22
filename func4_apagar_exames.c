2058d95680215973099533c8656ec244e4ec75c2
inesamaro <ines.amaro.316@gmail.com>
2017-05-22 17:17:59 +0100

    Update func4_apagar_exames.c

--- func4_apagar_exames.c
+++ func4_apagar_exames.c
@@ -1,28 +1,28 @@
-//versao 2 da Func4
-
+//ja funcionar
 void apagar_exames(Next_exame lista_exames) {
   Data data_presente, data_exame;
-  Next_exame aux = lista_exames, ant;
+  Next_exame l_exames, ant, actual;
   int res;
 
   printf("Qual é o dia de hoje?:\nDia: ");
-  scanf("%d", data_presente->dia);
+  scanf("%d", &(data_presente.dia));
   printf("Mês: ");
-  scanf("%d", data_presente->mes);
+  scanf("%d", &(data_presente.mes));
   printf("Ano: ");
-  scanf("%d", data_presente->ano);
+  scanf("%d", &(data_presente.ano));
 
-
-//isto nao vai funcionar porque nao temos o ponteiro ant para conseguirmos aceder ao atual
-  ant = aux;
-  while (aux != NULL) {
-    res = comparar_datas(data_presente, aux->data);
+  l_exames = lista_exames;
+  ant = l_exames;
+  actual = l_exames->next;
+  while(actual != NULL) {
+    printf("Data do exame actual: %d-%d-%d\n", actual->data.dia, actual->data.mes, actual->data.ano);
+    res = comparar_datas(data_presente, actual->data);
+    printf("res: %d\n", res);
     if (res == 1) {
-      ant->next = aux->next;
-      free(aux);
+      ant->next = actual->next;
+      free(actual);
     }
-    aux = aux->next;
-    ant = aux;
-
+    ant = ant->next;
+    actual = actual->next;
   }
 }
