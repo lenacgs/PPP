5d7e13f333e35e5657b556dc01100bb94dd6045f
lenacgs <lenacgs@gmail.com>
2017-05-24 19:44:45 +0100

    Update le_ficheiros.c

--- le_ficheiros.c
+++ le_ficheiros.c
@@ -5,20 +5,19 @@ void le_ficheiro_disciplinas (Next_disciplina lista_disciplinas) {
   Next_disciplina novaDisciplina;
   fp = fopen("ficheiro_disciplinas.txt", "r");
   char nome[100];
-
-  printf("Leitura do ficheiro disciplinas:\n");
-  while(fscanf(fp, "%[^,\n]", nome) != EOF) {
+  printf("LEITURA DA LISTA DE DISCIPLINAS:\n");
+  while(fscanf(fp, " %[^,\n]", nome) != EOF) {
     fseek(fp, 1, SEEK_CUR);
     novaDisciplina = (Next_disciplina)malloc(sizeof(Node_disciplina));
 
     novaDisciplina->nome = (char*)malloc(50*sizeof(char));
     strcpy(novaDisciplina->nome, nome);
-    printf("nome: %s", novaDisciplina->nome);
+    printf("NOME DA DISCIPLINA: %s\n", novaDisciplina->nome);
 
     novaDisciplina->docente = (char*)malloc(50*sizeof(char));
     fscanf(fp, "%[^,\n]", novaDisciplina->docente);
+    printf("DOCENTE DA DISCIPLINA: %s\n", novaDisciplina->docente);
     fseek(fp, 1, SEEK_CUR);
-    printf("docente: %s", novaDisciplina->docente);
 
     insere_disciplina(lista_disciplinas, novaDisciplina);
   }
@@ -67,17 +66,18 @@ void le_ficheiro_exames(Next_exame lista_exames, Next_disciplina lista_disciplin
   Next_exame novoExame;
   Next_disciplina l_disciplinas = lista_disciplinas->next;
   Next_aluno l_alunos = lista_alunos;
-  Next_ptrs_aluno new_node, l_inscritos;
+  Next_ptrs_aluno lista_inscritos, l_inscritos;
   fp = fopen("ficheiro_exames.txt", "r");
   char disciplina[100], c, string[100], id[100], s[2]= ",", *numero;
   int i, num, num_aluno;
+
 
   if (fp == NULL) {
     printf("Nao conseguiu abrir o ficheiro ->\"ficheiro_exames.txt\"<- \\\n");
     return;
   }
-  printf("Conseguiu abrir o ficheiro ->\"ficheiro_exames.txt\"<- \\\n");
-  printf("Leitura da lista de exames: \n");
+
+  printf("LEITURA DA LISTA DE EXAMES: \n");
 
   while(fscanf(fp, " %[^,]", id) != EOF) {
     l_disciplinas = lista_disciplinas->next;
@@ -85,23 +85,24 @@ void le_ficheiro_exames(Next_exame lista_exames, Next_disciplina lista_disciplin
     novoExame = (Next_exame)malloc(sizeof(Node_exame));
     fseek (fp, 1, SEEK_CUR);
     novoExame->id = atoi(id);
-    printf("Id: %d\n", novoExame->id);
+    printf("ID: %d\n", novoExame->id);
 
     novoExame->disciplina = (Next_disciplina)malloc(sizeof(Node_disciplina));
     fscanf(fp, "%[^,]", disciplina);
-    printf("Disciplina: %s\n", disciplina);
 
     while (l_disciplinas != NULL) {
+
       if (strcmp(l_disciplinas->nome, disciplina) == 0) {
         novoExame->disciplina = l_disciplinas;
       }
       l_disciplinas = l_disciplinas->next;
     }
+    printf("DISCIPLINA: %s\n", novoExame->disciplina->nome);
     fseek(fp, 1, SEEK_CUR);
 
     novoExame->epoca = (char*)malloc(50*sizeof(char));
     fscanf(fp, "%[^,\n]", novoExame->epoca);
-    printf("epoca: %s\n", novoExame->epoca);
+    printf("EPOCA: %s\n", novoExame->epoca);
     fseek(fp, 1, SEEK_CUR);
 
     fscanf(fp, "%d[^,\n]", &(novoExame->data.dia));
@@ -110,47 +111,44 @@ void le_ficheiro_exames(Next_exame lista_exames, Next_disciplina lista_disciplin
     fseek(fp, 1, SEEK_CUR);
     fscanf(fp, "%d[^,\n]", &(novoExame->data.ano));
     fseek(fp, 1, SEEK_CUR);
-    printf("data: %d.%d.%d\n", novoExame->data.dia, novoExame->data.mes, novoExame->data.ano);
+    printf("DATA: %d.%d.%d\n", novoExame->data.dia, novoExame->data.mes, novoExame->data.ano);
 
     fscanf(fp, "%d[^,\n]", &(novoExame->hora.horas));
     fseek(fp, 1, SEEK_CUR);
     fscanf(fp, "%d[^,\n]", &(novoExame->hora.mins));
     fseek(fp, 1, SEEK_CUR);
-    printf("horas: %dh %dmin\n", novoExame->hora.horas, novoExame->hora.mins);
+    printf("HORAS: %dh %dmin\n", novoExame->hora.horas, novoExame->hora.mins);
 
     fscanf(fp, "%d[^,\n]", &(novoExame->duracao));
     fseek(fp, 1, SEEK_CUR);
-    printf("duração: %d\n", novoExame->duracao);
+    printf("DURACAO: %d\n", novoExame->duracao);
 
     novoExame->sala = (char*)malloc(50*sizeof(char));
     fscanf(fp, "%[^,\n]", novoExame->sala);
-    printf("sala: %s\n", novoExame->sala);
+    printf("SALA: %s\n", novoExame->sala);
     fseek(fp, 1, SEEK_CUR);
 
-    novoExame->inscritos = cria_lista_inscritos();
-    l_inscritos = novoExame->inscritos;
+    lista_inscritos = cria_lista_inscritos();
+    l_inscritos = lista_inscritos;
 
     fscanf(fp, "%[^\n]", string);
 
-
     numero = strtok(string, s);
-    num = atoi(numero);
-    while(numero != NULL ) {
-      num = atoi(numero);
-      l_alunos = lista_alunos->next;
-      while (l_alunos != NULL) {
-        if  (l_alunos->num_aluno == num) {
+    num_aluno = atoi(numero);
+    while( numero != NULL ) {
+      num_aluno = atoi(numero);
+      while (l_alunos->next != NULL) {
+        if  (l_alunos->num_aluno == num_aluno) {
           while (l_inscritos->next != NULL) {
             l_inscritos = l_inscritos->next;
           }
-          new_node = (Next_ptrs_aluno)malloc(sizeof(Node_ptrs_aluno));
-          l_inscritos->next = new_node;
-          new_node->aluno = l_alunos;
-          new_node->next = NULL;
+          l_inscritos->next = (Next_ptrs_aluno)malloc(sizeof(Node_ptrs_aluno));
+          l_inscritos->next->aluno = l_alunos;
+          l_inscritos->next->next = NULL;
         }
         l_alunos = l_alunos->next;
       }
-
+      printf("Aluno inscrito: %d\n", num_aluno);
       numero = strtok(NULL, s);
     }
     insere_exame(lista_exames, novoExame);
