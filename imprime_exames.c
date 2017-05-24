54c23d3e86378c3aca0aa43397da718a6e535042
lenacgs <lenacgs@gmail.com>
2017-05-24 01:28:14 +0100

    Update imprime_exames.c

--- imprime_exames.c
+++ imprime_exames.c
@@ -2,10 +2,11 @@ void imprime_exames(Next_exame lista) {
   Next_exame l;
 
   l = lista->next;
+  printf("LISTA DE EXAMES EXISTENTES:\n");
   while(l != NULL) {
-    printf("\nnome: %d", l->id);
-    printf("\nnome: %s", l->disciplina->nome);
-    printf("\nepoca: %s", l->epoca);
+    printf("\nID: %d", l->id);
+    printf("\nnNOME: %s", l->disciplina->nome);
+    printf("\nEPOCA: %s", l->epoca);
     l = l->next;
   }
 }
