4e56a38255dcdff008e9f32234e82d1238e276f1
inesamaro <ines.amaro.316@gmail.com>
2017-05-25 22:53:00 +0100

    Update teste_func2_criar_disciplina.c

--- teste_func2_criar_disciplina.c
+++ teste_func2_criar_disciplina.c
@@ -93,18 +93,26 @@
     }
 
     void criar_disciplina(Next_disciplina lista_disciplinas) {
-        Next_disciplina novaDisciplina = (Next_disciplina)malloc(sizeof(Node_disciplina));
+        Next_disciplina novaDisciplina;
+        int n, i;
 
-        novaDisciplina->nome = (char*)malloc(50*sizeof(char));
-        printf("Nome da disciplina: ");
-        scanf("%s", novaDisciplina->nome);
+        printf("Quantas disciplinas e que quer criar? ");
+        scanf("%d", &n);
+        for(i=1; i<=n;i++) {
+          printf("Disciplina %d:\n", i);
+          novaDisciplina = (Next_disciplina)malloc(sizeof(Node_disciplina));
 
-        novaDisciplina->docente = (char*)malloc(50*sizeof(int));
-        printf("Nome do docente: ");
-        scanf("%s", novaDisciplina->docente);
+          novaDisciplina->nome = (char*)malloc(50*sizeof(char));
+          printf("Nome da disciplina: ");
+          scanf("%s", novaDisciplina->nome);
 
-        insere_disciplina(lista_disciplinas, novaDisciplina);
-        escreve_ficheiro_disciplinas(lista_disciplinas, novaDisciplina);
+          novaDisciplina->docente = (char*)malloc(50*sizeof(int));
+          printf("Nome do docente: ");
+          scanf("%s", novaDisciplina->docente);
+
+          insere_disciplina(lista_disciplinas, novaDisciplina);
+          escreve_ficheiro_disciplinas(lista_disciplinas, novaDisciplina);
+        }
     }
 
     Next_disciplina cria_lista_disciplinas() {
