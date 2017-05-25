1dbedcba35a63d31d5342a03eda6990256f5f57d
lenacgs <lenacgs@gmail.com>
2017-05-25 14:44:02 +0100

    Update verifica_sala.c

--- verifica_sala.c
+++ verifica_sala.c
@@ -1,19 +1,23 @@
 int verifica_sala(char *string, Next_exame lista_exames, Next_exame new_node) {
   int inicio, fim, inicio_nova, fim_nova, res;
-  Next_exame l = lista_exames->next;
+  Next_exame l_exames = lista_exames->next;
+  Next_sala l;;
 
-  while (l->next != NULL) {
-    inicio = (l->hora.horas)* 60 + (l->hora.mins);
-    fim = inicio + l->duracao;
+  while (l_exames->next != NULL) {
+    l = l_exames->salas;
+    inicio = (l_exames->hora.horas)* 60 + (l_exames->hora.mins);
+    fim = inicio + l_exames->duracao;
     inicio_nova = (new_node->hora.horas)*60 + (new_node->hora.mins);
     fim_nova = inicio_nova + new_node->duracao;
     //a sala ser igual
-    if (strcmp(string, l->sala)==0) {
-      //a data ser igual
-      if ((l->data.dia == new_node->data.dia) && (l->data.mes == new_node->data.mes) && (l->data.ano == new_node->data.ano)) {
-        //a hora coincidir
-        if ((inicio_nova>=inicio && inicio_nova<=fim) || (fim_nova>=inicio && fim_nova<=fim) || (inicio_nova<=inicio && fim_nova>=fim)) {
-          return 0;
+    while (l->next != NULL) {
+      if (strcmp(string, l->sala) == 0) {
+        //a data ser igual
+        if ((l_exames->data.dia == new_node->data.dia) && (l_exames->data.mes == new_node->data.mes) && (l_exames->data.ano == new_node->data.ano)) {
+          //a hora coincidir
+          if ((inicio_nova>=inicio && inicio_nova<=fim) || (fim_nova>=inicio && fim_nova<=fim) || (inicio_nova<=inicio && fim_nova>=fim)) {
+            return 0;
+          }
         }
       }
     }
