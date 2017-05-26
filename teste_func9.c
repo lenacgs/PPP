489a4251925805c569d1231f2d5f35ffee4f8e25
inesamaro <ines.amaro.316@gmail.com>
2017-05-26 01:43:13 +0100

    Create teste_func9.c

---
+++ teste_func9.c
@@ -0,0 +1,40 @@
+void verificar_salas_suficientes(Next_exame lista_exames) {
+  Next_exame l_exames = lista_exames->next;
+  Next_ptrs_aluno l_inscritos;
+  char nova_sala[50];
+  int capac_max, num_alunos, id, x;
+
+  printf("De qual dos exames e que quer saber se ha salas suficientes?\n");
+  imprime_exame(lista_exames);
+  printf("Id do exame que deseja: ");
+  scanf("%d", &id)
+  while (l_exames->id != id) {
+    l_exames = l_exames->next;
+  }
+
+  l_inscritos = l_exames->inscritos;
+  while (l_inscritos != NULL) {
+    num_alunos ++;
+  }
+
+  if (num_alunos <= capac_max)
+    printf("Este exame tem salas reservadas suficientes.\n")
+  else {
+    n = num_alunos/capac_max;
+    resto = num_alunos%capac_max;
+    if (resto != 0)
+      n++;
+    printf("Este exame não tem salas reservadas suficientes.\nSao necessarias mais %d salas:\n", n);
+    for(i=0; i<n; i++) {
+      printf("Sala %d: ",  i);
+      scanf("%s", nova_sala);
+      x = verifica_sala(sala, lista_exames, l_exames);
+      while (x != 1) {
+        getchar();
+        printf("\nA sala nao esta disponivel. Escolha outra sala: ");
+        scanf("%s", nova_sala);
+        res = verifica_sala(string, lista_exames, l_exames);
+      }
+    }
+  }
+}
