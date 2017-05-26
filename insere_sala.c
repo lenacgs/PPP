accd51846589b97da4b97487d40910c0cf9caa24
lenacgs <lenacgs@gmail.com>
2017-05-26 15:23:30 +0100

    Update insere_sala.c

--- insere_sala.c
+++ insere_sala.c
@@ -1,13 +1,14 @@
-//quando queremos adicionar uma nova sala a um determinado exame, ou seja, escrever a sala no fim da linha que tem como id o id do exame em causa
-
 void insere_sala(char *sala, Next_exame lista_exames, Next_exame exame) {
+  printf("Entrou na funcao insere_sala.\nVai inserir a sala %s no exame %d.\n", sala, exame->id);
   Next_sala l_salas = exame->salas;
-
+  printf("ola\n");
   while (l_salas->next != NULL) {
     l_salas = l_salas->next;
   }
+  printf("Ola2\n");
   //l_salas->next = NULL
   l_salas->next = (Next_sala)malloc(sizeof(Node_sala));
+  printf("ola3\n");
   l_salas->next->sala = sala;
   printf("sala: %s\n", l_salas->next->sala);
   l_salas->next->next = NULL;
