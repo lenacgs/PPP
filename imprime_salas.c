7b5b4ccf4dd1ce09611ca896400a0aed327043f0
lenacgs <lenacgs@gmail.com>
2017-05-27 18:57:43 +0100

    Update imprime_salas.c

--- imprime_salas.c
+++ imprime_salas.c
@@ -2,7 +2,7 @@ void imprime_listas_salas(Next_exame lista_exames) { //imprime todas as salas de
   Next_sala lista_salas;
   Next_exame l_exames = lista_exames->next;
 
-  while (l_exames->next != NULL) {
+  while (l_exames != NULL) {
     printf("O exame com ID %d, da disciplina %s em época %s tem reservadas as seguintes salas:\n", l_exames->id, l_exames->disciplina->nome, l_exames->epoca);
     lista_salas = l_exames->salas->next;
     while (lista_salas != NULL) {
