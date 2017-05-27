60d1b17a6fb8c378c941d7238f21cd3fb35c7e93
inesamaro <ines.amaro.316@gmail.com>
2017-05-27 16:05:37 +0100

    Update imprime_exames.c

--- imprime_exames.c
+++ imprime_exames.c
@@ -1,9 +1,9 @@
 void imprime_exames(Next_exame lista_exames) {
   Next_exame l_exames;
-  
-  l_exames = lista->next;
+
+  l_exames = lista_exames->next;
   while(l_exames != NULL) {
-    printf("Id: %d, %s, %s\n", l->id, l->disciplina->nome, l->epoca);
+    printf("Id: %d, %s, %s\n", l_exames->id, l_exames->disciplina->nome, l_exames->epoca);
     l_exames = l_exames->next;
   }
 }
