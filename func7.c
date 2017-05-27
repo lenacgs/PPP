4e7f52379c5d0e2351d529c92b2aa6fdc1389025
inesamaro <ines.amaro.316@gmail.com>
2017-05-27 16:09:20 +0100

    Update func7.c

--- func7.c
+++ func7.c
@@ -1,32 +1,25 @@
-//ja acabada so falta mudar a escolha do exame. devia ser por id e não por numero pelo qual é listado
-
+//TUDINHO A FUNCIONAR, ESCOLHE O EXAME PELO ID
 void listar_alunos(Next_exame lista_exames) {
-  Next_exame l_exames = lista_exames->next; //por causa do cabeçalho
-  Next_ptrs_aluno inscritos;
+  Next_exame l_exames; //por causa do cabeçalho
+  Next_ptrs_aluno l_inscritos;
   char *ptr, c;
-  int i=1, num;
+  int i=1, id;
   printf("Seleccione o exame do qual quer listar os alunos: \n");
-  while (l_exames) {
-    printf("%d. Id: %d, ", i, l_exames->id);
-    printf("%s ", l_exames->disciplina->nome);
-    printf("%s\n", l_exames->epoca);
-    l_exames = l_exames->next;
-    i++;
-  }
+  imprime_exames(lista_exames);
 
-  l_exames = lista_exames;
-  printf("\nOpcao: ");
-  scanf("%d", &num);
-  for (i=0; i<num; i++) {
+  l_exames = lista_exames->next;
+  printf("Id do exame que deseja: ");
+  scanf("%d", &id);
+  while(l_exames->id != id) {
     l_exames = l_exames->next;
   }
-  printf("Exame me que estamos: %d\n", l_exames->id);
 
+  printf("Id do exame me que estamos: %d\n", l_exames->id);
   printf("Alunos inscritos no exame:\n");
-  inscritos = l_exames->inscritos->next;
-  while (inscritos != NULL) {
-    printf("%d. %d\n", i, inscritos->aluno->num_aluno);
+  l_inscritos = l_exames->inscritos->next;
+  while (l_inscritos != NULL) {
+    printf("%d. %d\n", i, l_inscritos->aluno->num_aluno);
     i++;
-    inscritos = inscritos->next;
+    l_inscritos = l_inscritos->next;
   }
 }
