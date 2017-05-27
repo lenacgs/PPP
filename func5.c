84fa9254b204a1182dd737d43c43e8e0208b55c5
inesamaro <ines.amaro.316@gmail.com>
2017-05-27 14:19:45 +0100

    Update func5.c

--- func5.c
+++ func5.c
@@ -1,58 +1,57 @@
-//ja acabada so falta mudar a escolha do exame. devia ser por id e não por numero pelo qual é listado
+//TODINHA A FUNCIONAR, A ESCOLHA DO EXAME JA E FEITA PELO ID
+void listar_exames(Next_exame lista_exames) {
+  Next_exame l_exames;
+  int i=1, id;
+  Next_ptrs_aluno l_inscritos;
+  Next_sala l_salas;
 
-void imprimir_exames(Next_exame lista) {
-  Next_exame l, ant, actual;
-  int i=1, c;
-  char *ptr, *disc, *ep, *doc;
-  Next_ptrs_aluno inscritos;
-
-  l = lista->next;
+  l_exames = lista_exames->next;
   printf("Lista de exames existentes:\n");
-  while (l) { //lista os exames existentes na lista, juntamente com a epoca
-    printf("%d. %s", i, l->disciplina->nome);
-    i++;
-    printf(", %s\n", l->epoca);
-    l = l->next;
-  }
+  imprime_exames(lista_exames);
+
   /*o exame devia ser identificado pelo id*/
-  printf("\nEscolha o exame: ");
-  scanf("%d", &c);
+  printf("\nEscolha o id do exame: ");
+  scanf("%d", &id);
 
-  l = lista;
-  for (i=1; i<=c; i++) {
-    l = l->next;
-    printf("ciclo:\n");
-    printf("id: %d\n", l->id);
+  l_exames = lista_exames->next;
+  while (l_exames->id != id) {
+    l_exames = l_exames->next;
   }
 
-  printf("Informacao desse exame:\n ");
-  printf("\nDisciplina: ");
-  printf("%s", l->disciplina->nome);
+  printf("Informacao desse exame:\n");
+  printf("Disciplina: ");
+  printf("%s\n", l_exames->disciplina->nome);
 
-  printf("\nDocente: ");
-  printf("%s", l->disciplina->docente);
+  printf("Docente: ");
+  printf("%s\n", l_exames->disciplina->docente);
 
-  printf("\nÉpoca: ");
-  printf("%s", l->epoca);
+  printf("Época: ");
+  printf("%s\n", l_exames->epoca);
 
-  printf("\nData: ");
-  printf("%d - %d - %d", l->data.dia, l->data.mes, l->data.ano);
+  printf("Data: ");
+  printf("%d-%d-%d\n", l_exames->data.dia, l_exames->data.mes, l_exames->data.ano);
 
-  printf("\nHora: ");
-  printf("%dh%d", l->hora.horas, l->hora.mins);
+  printf("Hora: ");
+  printf("%dh%d\n", l_exames->hora.horas, l_exames->hora.mins);
 
-  printf("\nSala: ");
-  printf("%s", l->sala);
+  printf("Salas: \n");
+  l_salas = l_exames->salas->next;
+  while (l_salas != NULL) {
+    printf("Sala %d: %s\n", i, l_salas->sala); //imprime null
+    i++;
+    l_salas = l_salas->next;
+  }
 
-  printf("\nDuração: %d minutos", l->duracao);
+  printf("Duração: %d minutos\n", l_exames->duracao);
 
-  printf("\nAlunos inscritos: \n");
+  printf("Alunos inscritos: \n");
 
   i=1;
-  inscritos = l->inscritos->next;
-  while (inscritos != NULL) {
-    printf("%d. %d\n", i, inscritos->aluno->num_aluno);
+  l_inscritos = l_exames->inscritos->next;
+  while (l_inscritos != NULL) {
+    printf("%d. %d\n", i, l_inscritos->aluno->num_aluno);
     i++;
-    inscritos = inscritos->next;
+    l_inscritos = l_inscritos->next;
   }
 }
+
