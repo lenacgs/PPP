a66dc230682db4c42e6af01fa7026956aae40c96
lenacgs <lenacgs@gmail.com>
2017-05-27 17:06:26 +0100

    Update verifica_sala.c

--- verifica_sala.c
+++ verifica_sala.c
@@ -1,17 +1,22 @@
 int verifica_sala(char *string, Next_exame lista_exames, Next_exame new_node) {
-  int inicio, fim, inicio_nova, fim_nova, res;
+  int inicio, fim, inicio_nova, fim_nova;
   Next_exame l_exames = lista_exames->next;
-  Next_sala l;
+  Next_sala l_salas;
 
-  while (l_exames->next != NULL) {
-    l = l_exames->salas;
+  printf("Comecou a func verifica_sala\n");
+
+  while (l_exames != NULL) {
+    printf("Entrou dentro de um novo node_exame\n");
+    l_salas = l_exames->salas->next;
+    //calcular os inicios e fins em minutos
     inicio = (l_exames->hora.horas)* 60 + (l_exames->hora.mins);
     fim = inicio + l_exames->duracao;
     inicio_nova = (new_node->hora.horas)*60 + (new_node->hora.mins);
     fim_nova = inicio_nova + new_node->duracao;
     //a sala ser igual
-    while (l->next != NULL) {
-      if (strcmp(string, l->sala) == 0) {
+    while (l_salas != NULL) {
+      printf("A nova sala %s vai ser comparada com a sala %s do exame %d.\n", string, l_salas->sala, l_exames->id);
+      if (strcmp(string, l_salas->sala) == 0) {
         //a data ser igual
         if ((l_exames->data.dia == new_node->data.dia) && (l_exames->data.mes == new_node->data.mes) && (l_exames->data.ano == new_node->data.ano)) {
           //a hora coincidir
@@ -20,8 +25,10 @@ int verifica_sala(char *string, Next_exame lista_exames, Next_exame new_node) {
           }
         }
       }
+      l_salas = l_salas->next;
     }
-    l = l->next;
+    l_exames = l_exames->next;
   }
+  printf("A sala não está ocupada\n");
   return 1;
 }
